ff296670a19c63c918d5e432ce03b8a969de715b
NovbiChannel <chaglovnikita@gmail.com>
2022-12-05 18:09:11 +0500

    I,m setup the new Dagger-Hilt library so i,m are able to inject dependencies into my classes. That will make my code much cleaner.

--- app/src/main/java/com/example/runningapp/MainActivity.kt
+++ app/src/main/java/com/example/runningapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.runningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.runningapp.db.RunDao
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDao: RunDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("runDao", "RUNDAO: ${runDao.hashCode()}")
     }
 }
---
+++ app/src/main/java/com/example/runningapp/other/Constatns.kt
@@ -0,0 +1,6 @@
+package com.example.runningapp.other
+
+object Constatns {
+
+    const val RUNNING_DATABASE_NAME = "running_db"
+}
